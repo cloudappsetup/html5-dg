6d9057336adb1908422a32b3921412bb67eab1ea
Jonathan LeBlanc <joncleblanc@yahoo.com>
2011-10-10 11:47:10 -0700

    fixed #4 - adjusted cancel callback

--- server/php/cancel.php
+++ server/php/cancel.php
@@ -3,11 +3,18 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Purchase Cancelled</title>
+<script>
+
+function closeFlow() {
+    parent.pptransact.releaseDG();
+}
+</script>
 </head>
 
-<body>
+<body onload="closeFlow()">
 <div style="background-color:#FFF;height:400px;width:300px; border-radius:8px;padding:20px;">
     Purchase Cancelled
+    <button id="close" onclick="closeFlow();">close</button>
 </div>
 </body>
 </html>
